1a735d83b814158156c3edc20c052bbf8e602fa4
peters <peter.stritzinger@abf.at>
2022-05-24 13:25:12 +0200

    #1 created spring based project

---
+++ src/main/java/org/openindustryautomation/i18n/openI18n/OpenI18nApplication.java
@@ -0,0 +1,13 @@
+package org.openindustryautomation.i18n.openI18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenI18nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenI18nApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/openindustryautomation/i18n/openI18n/OpenI18nApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openindustryautomation.i18n.openI18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenI18nApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
